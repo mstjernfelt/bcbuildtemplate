fa4ae47dbe26ed7bdedd4db4b120cf2c0508983e
Mathias Stjernfelt <mathias@brightcom.se>
2022-06-19 21:35:24 +0200

    Update

--- scripts/Run-ExtensionPSScript.ps1
+++ scripts/Run-ExtensionPSScript.ps1
@@ -35,8 +35,6 @@ foreach ($extension in $settings.scriptExtension) {
         }
     }
 
-    $parameters = $extension.parameters | ConvertTo-Json
-
     Write-Host "Executing custom PS script $($extension.path) on task $($ENV:SYSTEM_TASKDISPLAYNAME)"
     . $ExtensionScript $parameters
 }
