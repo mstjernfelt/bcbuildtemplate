94690cb8762383e48610a211d5d3e3af99de48cc
Mathias Stjernfelt <mathias@brightcom.se>
2022-06-19 21:02:56 +0200

    Update

--- scripts/Run-ExtensionPSScript.ps1
+++ scripts/Run-ExtensionPSScript.ps1
@@ -3,7 +3,7 @@ Param(
     [string] $configurationFilePath
 )
 
-$settings = (Get-Content -Path $configurationFilePath -Encoding UTF8 | Out-String | ConvertFrom-Json)
+$settings = (Get-Content -Path $configurationFilePath -Encoding UTF8 | Out-String | ConvertFrom-Json -AsHashtable)
 
 $ExtensionScript = Join-Path $env:TEMP 'ExtensionScript.ps1'
 
@@ -12,35 +12,31 @@ if (Test-Path $ExtensionScript) {
   }
 
 foreach ($extension in $settings.scriptExtension) {
-    Write-Host "Fetching custom script $($extension.path)"
+    Write-Host "Fetching custom script $($extension['path'])"
 
-    $execParams = $extension.parameters | ConvertFrom-Json
-
-    $execParams
-
-    if ($extension.TaskName -ne $ENV:SYSTEM_TASKDISPLAYNAME) {
-        Write-Host "No custom PS script matches TaskName $($extension.TaskName) ($($ENV:SYSTEM_TASKDISPLAYNAME))"
+    if ($extension['taskName'] -ne $ENV:SYSTEM_TASKDISPLAYNAME) {
+        Write-Host "No custom PS script matches TaskName $($extension['taskName']) ($($ENV:SYSTEM_TASKDISPLAYNAME))"
         continue
     }
 
-    switch ($extension.path) {
+    switch ($extension['path']) {
         {$_ -like "*https://raw.githubusercontent.com*"} { 
-            Set-Content -Path $ExtensionScript -Value (Invoke-WebRequest -Uri $extension.path).Content -Encoding UTF8 -Force
+            Set-Content -Path $ExtensionScript -Value (Invoke-WebRequest -Uri $extension['path']).Content -Encoding UTF8 -Force
         }
         {$_ -like "*https://dev.azure.com/*"} {
             $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
             $headers.Add("Authorization", "Basic OnRoZ2Rmcmtmc2p4bHA1ZmRrdWFjdGN6cm5rNW1oYXR0dG1ya2Z5bDJzcm1qNWpuNjV1Z2E=")
             $headers.Add("Cookie", "VstsSession=%7B%22PersistentSessionId%22%3A%220bea08cd-0e83-4afd-bc1f-6778bff9b093%22%2C%22PendingAuthenticationSessionId%22%3A%2200000000-0000-0000-0000-000000000000%22%2C%22CurrentAuthenticationSessionId%22%3A%2200000000-0000-0000-0000-000000000000%22%2C%22SignInState%22%3A%7B%7D%7D")
        
-            Set-Content -Path $ExtensionScript -Value (Invoke-WebRequest -Uri $extension.path -Method 'GET' -Headers $headers).Content -Encoding UTF8 -Force
+            Set-Content -Path $ExtensionScript -Value (Invoke-WebRequest -Uri $extension['path'] -Method 'GET' -Headers $headers).Content -Encoding UTF8 -Force
         }
         Default {
-            Set-Content -Path $ExtensionScript -Value (Get-Content -Path $extension.path) -Encoding UTF8 -Force
+            Set-Content -Path $ExtensionScript -Value (Get-Content -Path $extension['path']) -Encoding UTF8 -Force
         }
     }
 
-    Write-Host $execParams.GetType()
+    Write-Host $extension['parameters'].GetType()
 
-    Write-Host "Executing custom PS script $($extension.path) on task $($ENV:SYSTEM_TASKDISPLAYNAME)"
-    . $ExtensionScript $execParams
+    Write-Host "Executing custom PS script $($extension['path']) on task $($ENV:SYSTEM_TASKDISPLAYNAME)"
+    . $ExtensionScript $extension['parameters']
 }
