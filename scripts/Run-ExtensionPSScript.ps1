e71caada2abf0e4ce3cd469becbe778ccf6d5eca
Mathias Stjernfelt <mathias@brightcom.se>
2022-06-28 16:55:53 +0200

    Update

--- scripts/Run-ExtensionPSScript.ps1
+++ scripts/Run-ExtensionPSScript.ps1
@@ -26,15 +26,14 @@ foreach ($extension in $settings.scriptExtension) {
         {$_ -like "*https://dev.azure.com/*"} {
             $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
             $headers.Add("Authorization", "Basic OnRoZ2Rmcmtmc2p4bHA1ZmRrdWFjdGN6cm5rNW1oYXR0dG1ya2Z5bDJzcm1qNWpuNjV1Z2E=")
-            $headers.Add("Cookie", "VstsSession=%7B%22PersistentSessionId%22%3A%220bea08cd-0e83-4afd-bc1f-6778bff9b093%22%2C%22PendingAuthenticationSessionId%22%3A%2200000000-0000-0000-0000-000000000000%22%2C%22CurrentAuthenticationSessionId%22%3A%2200000000-0000-0000-0000-000000000000%22%2C%22SignInState%22%3A%7B%7D%7D")
        
             Set-Content -Path $ExtensionScript -Value (Invoke-WebRequest -Uri $extension.path -Method 'GET' -Headers $headers).Content -Encoding UTF8 -Force
         }
         Default {
             Set-Content -Path $ExtensionScript -Value (Get-Content -Path $extension.path) -Encoding UTF8 -Force
-        }
+        }        
     }
 
     Write-Host "Executing custom PS script $($extension.path) on task $($ENV:SYSTEM_TASKDISPLAYNAME)"
-    . $ExtensionScript $parameters
+    . $ExtensionScript $extension.parameters
 }
