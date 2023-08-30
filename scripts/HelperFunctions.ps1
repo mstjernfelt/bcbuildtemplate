c455b6c92bcc145316e2805a608ee2ddbaf76bd8
Mathias Stjernfelt <mathias@brightcom.se>
2023-08-30 15:30:13 +0200

    save

--- scripts/HelperFunctions.ps1
+++ scripts/HelperFunctions.ps1
@@ -144,3 +144,63 @@ function Download-File2 {
         TrackTrace -telemetryScope $telemetryScope
     }
 }
+
+function GetExtendedErrorMessage {
+    Param(
+        $errorRecord
+    )
+
+    $exception = $errorRecord.Exception
+    $message = $exception.Message
+
+    try {
+        if ($errorRecord.ErrorDetails) {
+            $errorDetails = $errorRecord.ErrorDetails | ConvertFrom-Json
+            $message += " $($errorDetails.error)`r`n$($errorDetails.error_description)"
+        }
+    }
+    catch {}
+    try {
+        if ($exception -is [System.Management.Automation.MethodInvocationException]) {
+            $exception = $exception.InnerException
+        }
+        if ($exception -is [System.Net.Http.HttpRequestException]) {
+            $message += "`r`n$($exception.Message)"
+            if ($exception.InnerException) {
+                if ($exception.InnerException -and $exception.InnerException.Message) {
+                    $message += "`r`n$($exception.InnerException.Message)"
+                }
+            }
+
+        }
+        else {
+            $webException = [System.Net.WebException]$exception
+            $webResponse = $webException.Response
+            try {
+                if ($webResponse.StatusDescription) {
+                    $message += "`r`n$($webResponse.StatusDescription)"
+                }
+            }
+            catch {}
+            $reqstream = $webResponse.GetResponseStream()
+            $sr = new-object System.IO.StreamReader $reqstream
+            $result = $sr.ReadToEnd()
+        }
+        try {
+            $json = $result | ConvertFrom-Json
+            $message += "`r`n$($json.Message)"
+        }
+        catch {
+            $message += "`r`n$result"
+        }
+        try {
+            $correlationX = $webResponse.GetResponseHeader('ms-correlation-x')
+            if ($correlationX) {
+                $message += " (ms-correlation-x = $correlationX)"
+            }
+        }
+        catch {}
+    }
+    catch {}
+    $message
+}
