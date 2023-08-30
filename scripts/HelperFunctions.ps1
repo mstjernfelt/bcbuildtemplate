55974ffc6777535de0a12704d456a15a126ed374
Mathias Stjernfelt <mathias@brightcom.se>
2023-08-30 15:23:42 +0200

    save

--- scripts/HelperFunctions.ps1
+++ scripts/HelperFunctions.ps1
@@ -11,38 +11,136 @@ Function Get-BlobFromPrivateAzureStorageOauth2 {
         [String]$az_storage_clientSecret
     )
 
-    $tokenUrl = "https://login.microsoftonline.com/$az_storage_tenantId/oauth2/token"
+    Write-Host "Getting new Auth Context"
+    $context = New-BcAuthContext -tenantID $az_storage_tenantId -clientID $az_storage_clientId -clientSecret $az_storage_clientSecret -scopes "https://storage.azure.com/.default"
+    Write-Host "Access token retieved"
 
-    $tokenParams = @{
-        grant_type    = "client_credentials"
-        client_id     = $az_storage_clientId
-        client_secret = $az_storage_clientSecret
-        resource      = "https://storage.azure.com"
+    $headers = @{ 
+        "Authorization" = "Bearer $($context.accessToken)"
+        "x-ms-version"  = "2017-11-09"
     }
 
-    $tokenResponse = Invoke-RestMethod -Method POST -Uri $tokenUrl -ContentType "application/x-www-form-urlencoded" -Body $tokenParams
-    $accessToken = $tokenResponse.access_token
+    $TempFile = New-TemporaryFile
 
-    if (-not [string]::IsNullOrEmpty($accessToken)) {
-        $headers = @{
-            Authorization  = "Bearer $accessToken"
-            "x-ms-version" = "2017-11-09"
-            ContentType  = "text/plain"
+    Write-Host "Downloading $($parameters.licenseFile) to $($TempFile)"
+
+    Download-File2 -sourceUrl $blobUri -destinationFile $TempFile -headers $headers
+
+    $parameters.licenseFile = $TempFile
+
+    Write-Host "License file to use is $($TempFile)"
+
+    return($TempFile)
+}
+
+<#
+ .Synopsis
+  Download File
+ .Description
+  Download a file to local computer
+ .Parameter sourceUrl
+  Url from which the file will get downloaded
+ .Parameter destinationFile
+  Destinatin for the downloaded file
+ .Parameter dontOverwrite
+  Specify dontOverwrite if you want top skip downloading if the file already exists
+ .Parameter timeout
+  Timeout in seconds for the download
+ .Example
+  Download-File -sourceUrl "https://myurl/file.zip" -destinationFile "c:\temp\file.zip" -dontOverwrite
+#>
+function Download-File2 {
+    Param (
+        [Parameter(Mandatory = $true)]
+        [string] $sourceUrl,
+        [Parameter(Mandatory = $true)]
+        [string] $destinationFile,
+        [hashtable] $headers = @{"UserAgent" = "BcContainerHelper $bcContainerHelperVersion" },
+        [switch] $dontOverwrite,
+        [int]    $timeout = 100
+    )
+
+    function ReplaceCDN {
+        Param(
+            [string] $sourceUrl
+        )
+
+        $cdnStr = '.azureedge.net'
+        if ($sourceUrl -like "https://bcartifacts$cdnStr/*" -or $sourceUrl -like "https://bcinsider$cdnStr/*" -or $sourceUrl -like "https://bcprivate$cdnStr/*" -or $sourceUrl -like "https://bcpublicpreview$cdnStr/*") {
+            $idx = $sourceUrl.IndexOf("$cdnStr/", [System.StringComparison]::InvariantCultureIgnoreCase)
+            $sourceUrl = $sourceUrl.Substring(0, $idx) + '.blob.core.windows.net' + $sourceUrl.Substring($idx + $cdnStr.Length)
         }
-        try {
-            $TempFile = New-TemporaryFile
-            $response = Invoke-RestMethod -Method Get -Uri $blobUri -Headers $headers
-            Set-Content -Path $TempFile -Value $response -NoNewline
+        $sourceUrl
+    }
 
-            Write-Host "Successfully downloaded $($blobUri) from Azure Storage Container to $($TempFile)"
+    $telemetryScope = InitTelemetryScope -name $MyInvocation.InvocationName -parameterValues $PSBoundParameters -includeParameters @()
+    try {
 
-            return($TempFile.FullName)
+        $replaceUrls = @{
+            "https://go.microsoft.com/fwlink/?LinkID=844461"                                                                = "https://bcartifacts.azureedge.net/prerequisites/DotNetCore.1.0.4_1.1.1-WindowsHosting.exe"
+            "https://download.microsoft.com/download/C/9/E/C9E8180D-4E51-40A6-A9BF-776990D8BCA9/rewrite_amd64.msi"          = "https://bcartifacts.azureedge.net/prerequisites/rewrite_2.0_rtw_x64.msi"
+            "https://download.microsoft.com/download/5/5/3/553C731E-9333-40FB-ADE3-E02DC9643B31/OpenXMLSDKV25.msi"          = "https://bcartifacts.azureedge.net/prerequisites/OpenXMLSDKv25.msi"
+            "https://download.microsoft.com/download/A/1/2/A129F694-233C-4C7C-860F-F73139CF2E01/ENU/x86/ReportViewer.msi"   = "https://bcartifacts.azureedge.net/prerequisites/ReportViewer.msi"
+            "https://download.microsoft.com/download/1/3/0/13089488-91FC-4E22-AD68-5BE58BD5C014/ENU/x86/SQLSysClrTypes.msi" = "https://bcartifacts.azureedge.net/prerequisites/SQLSysClrTypes.msi"
+            "https://download.microsoft.com/download/3/A/6/3A632674-A016-4E31-A675-94BE390EA739/ENU/x64/sqlncli.msi"        = "https://bcartifacts.azureedge.net/prerequisites/sqlncli.msi"
+            "https://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe"           = "https://bcartifacts.azureedge.net/prerequisites/vcredist_x86.exe"
         }
-        catch {
-            Write-Error "An error occurred while downloading $($blobUri): $($_.Exception.Message)"
+
+        if ($replaceUrls.ContainsKey($sourceUrl)) {
+            $sourceUrl = $replaceUrls[$sourceUrl]
+        }
+
+        # If DropBox URL with dl=0 - replace with dl=1 (direct download = common mistake)
+        if ($sourceUrl.StartsWith("https://www.dropbox.com/", "InvariantCultureIgnoreCase") -and $sourceUrl.EndsWith("?dl=0", "InvariantCultureIgnoreCase")) {
+            $sourceUrl = "$($sourceUrl.Substring(0, $sourceUrl.Length-1))1"
+        }
+
+        if (Test-Path $destinationFile -PathType Leaf) {
+            if ($dontOverwrite) { 
+                return
+            }
+            Remove-Item -Path $destinationFile -Force
+        }
+        $path = [System.IO.Path]::GetDirectoryName($destinationFile)
+        if (!(Test-Path $path -PathType Container)) {
+            New-Item -Path $path -ItemType Directory -Force | Out-Null
+        }
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+        Write-Host "Downloading $destinationFile"
+        if ($sourceUrl -like "https://*.sharepoint.com/*download=1*") {
+            Invoke-WebRequest -UseBasicParsing -Uri $sourceUrl -OutFile $destinationFile
+        }
+        else {
+            if ($bcContainerHelperConfig.DoNotUseCdnForArtifacts) {
+                $sourceUrl = ReplaceCDN -sourceUrl $sourceUrl
+            }
+            try {
+                DownloadFileLow -sourceUrl $sourceUrl -destinationFile $destinationFile -dontOverwrite:$dontOverwrite -timeout $timeout -headers $headers
+            }
+            catch {
+                try {
+                    $waittime = 2 + (Get-Random -Maximum 5 -Minimum 0)
+                    $newSourceUrl = ReplaceCDN -sourceUrl $sourceUrl
+                    if ($sourceUrl -eq $newSourceUrl) {
+                        Write-Host "Error downloading..., retrying in $waittime seconds..."
+                    }
+                    else {
+                        Write-Host "Could not download from CDN..., retrying from blob storage in $waittime seconds..."
+                    }
+                    Start-Sleep -Seconds $waittime
+                    DownloadFileLow -sourceUrl $newSourceUrl -destinationFile $destinationFile -dontOverwrite:$dontOverwrite -timeout $timeout -headers $headers
+                }
+                catch {
+                    throw (GetExtendedErrorMessage $_)
+                }
+            }
         }
     }
-    else {
-        Write-Error "Failed to retrieve access token from $tokenUrl."
+    catch {
+        TrackException -telemetryScope $telemetryScope -errorRecord $_
+        throw
+    }
+    finally {
+        TrackTrace -telemetryScope $telemetryScope
     }
 }
