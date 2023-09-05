23ef3115e9f6bf4d2413d3ee3fd49cb4386d8496
Mathias Stjernfelt <mathias@brightcom.se>
2023-09-05 11:09:58 +0200

    save

--- scripts/HelperFunctions.ps1
+++ scripts/HelperFunctions.ps1
@@ -11,6 +11,10 @@ Function Get-BlobFromPrivateAzureStorageOauth2 {
         [String]$az_storage_clientSecret
     )
 
+    $az_storage_tenantId = ConvertSecureString($az_storage_tenantId)
+    $az_storage_clientId = ConvertSecureString($az_storage_clientId)
+    $az_storage_clientSecret = ConvertSecureString($az_storage_clientSecret)
+
     Write-Host "Getting new Auth Context"
     $context = New-BcAuthContext -tenantID $az_storage_tenantId -clientID $az_storage_clientId -clientSecret $az_storage_clientSecret -scopes "https://storage.azure.com/.default"
     Write-Host "Access token retieved"
@@ -31,4 +35,14 @@ Function Get-BlobFromPrivateAzureStorageOauth2 {
     Write-Host "License file to use is $($TempFile)"
 
     return($TempFile)
+}
+
+Function ConvertSecureString($secureString) {
+    if (Test-WSMan -Credential $secureString) {
+        Write-Host "The string is secure, converting."
+        $secureString = ([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)))
+        return $secureString
+    } else {
+        Write-Host "The string not secure."
+    }
 }
