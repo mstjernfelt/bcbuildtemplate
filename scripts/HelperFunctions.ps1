d5f1aec898a54705885e4ca1c631f06bfdb000f9
Mathias Stjernfelt <mathias@brightcom.se>
2023-09-05 14:10:12 +0200

    save

--- scripts/HelperFunctions.ps1
+++ scripts/HelperFunctions.ps1
@@ -12,7 +12,7 @@ Function Get-BlobFromPrivateAzureStorageOauth2 {
     )
 
     Write-Host "Getting new Auth Context"
-    $context = New-BcAuthContext -tenantID $ENV:AZSTORAGETENANTID -clientID $ENV:AZSTORAGETENANTID -clientSecret $ENV:AZSTORAGECLIENTSECRET -scopes "https://storage.azure.com/.default"
+    $context = New-BcAuthContext -tenantID $ENV:AZSTORAGETENANTID -clientID $ENV:AZSTORAGECLIENTID -clientSecret $ENV:AZSTORAGECLIENTSECRET -scopes "https://storage.azure.com/.default"
     Write-Host "Access token retieved"
 
     $headers = @{ 
