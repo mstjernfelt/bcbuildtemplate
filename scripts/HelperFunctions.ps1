a907dd02a2da92a12b824b680e7e16da9b5d3bf1
Mathias Stjernfelt <mathias@brightcom.se>
2023-09-06 12:07:02 +0200

    save

--- scripts/HelperFunctions.ps1
+++ scripts/HelperFunctions.ps1
@@ -11,9 +11,6 @@ Function Get-BlobFromPrivateAzureStorageOauth2 {
         [String]$az_storage_clientSecret
     )
 
-    $encodedSecret = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($az_storage_clientId))
-    Write-Host "Encoded Secret: $encodedSecret"
-
     Write-Host "Getting new Auth Context"
     $context = New-BcAuthContext -tenantID $ENV:AZSTORAGETENANTID -clientID $ENV:AZSTORAGECLIENTID -clientSecret $ENV:AZSTORAGECLIENTSECRET -scopes "https://storage.azure.com/.default"
     Write-Host "Access token retieved"
@@ -24,21 +21,13 @@ Function Get-BlobFromPrivateAzureStorageOauth2 {
     $headers = @{ 
         "Authorization" = "Bearer $($context.accessToken)"
         "x-ms-version"  = "2017-11-09"
+        "x-ms-date" = "$formattedDateTime"        
         "Content-Type" = "application/json"
-        "x-ms-date" = "$formattedDateTime"
     }
 
     $TempFile = New-TemporaryFile
 
-    Write-Host "Downloading $blobUri to $TempFile"
-
-    $hdr = $headers | ConvertTo-Json
-
-    Write-Host "Headers: $hdr"
-
     Download-File -sourceUrl $blobUri -destinationFile $TempFile -headers $headers
-
-    Write-Host "License file to use is $($TempFile)"
 
     return($TempFile)
 }
